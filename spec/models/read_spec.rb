9580f851c16d3a6a983a7c19cf8758bfd2130c5a
glassjoseph <glassjoseph@gmail.com>
2017-07-27 14:14:26 -0600

    Add request test for top_ten endpoint

--- spec/models/read_spec.rb
+++ spec/models/read_spec.rb
@@ -3,14 +3,11 @@ require 'rails_helper'
 RSpec.describe Read, type: :model do
   it "is invalid without a url" do
     read = Read.new
-
     expect(read).to be_invalid
   end
 
   it "is valid with a url" do
     read = Read.new(url: "https://www.turing.io")
-
     expect(read).to be_valid
   end
-
 end
