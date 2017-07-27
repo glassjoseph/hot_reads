c0d8809b6b2eff4f6d624c1696685cfe7cfeac6f
glassjoseph <glassjoseph@gmail.com>
2017-07-27 14:53:34 -0600

    Add test for Read.top_links

--- spec/models/read_spec.rb
+++ spec/models/read_spec.rb
@@ -10,4 +10,33 @@ RSpec.describe Read, type: :model do
     read = Read.new(url: "https://www.turing.io")
     expect(read).to be_valid
   end
+
+  it "can return top ten read links" do
+    10.times do
+      Read.create(url: "https://www.turing.io")
+    end
+    9.times do
+      Read.create(url: "https://www.google.com")
+    end
+    Read.create(url: "https://www.wikipedia.com")
+    Read.create(url: "https://www.wikipedia.com")
+    Read.create(url: "https://www.wikipedia.com")
+    Read.create(url: "https://www.amazon.com")
+    Read.create(url: "https://www.amazon.com")
+    Read.create(url: "https://www.facebook.com")
+    Read.create(url: "https://www.random1.com")
+    Read.create(url: "https://www.random2.com")
+    Read.create(url: "https://www.random3.com")
+    Read.create(url: "https://www.random4.com")
+    Read.create(url: "https://www.random5.com")
+    Read.create(url: "https://www.random6.com")
+
+    top_links = Read.top_links
+
+    expect(top_links.class).to eq(Array)
+    expect(top_links[0]).to eq("https://www.turing.io")
+    expect(top_links[1]).to eq("https://www.google.com")
+    expect(top_links[2]).to eq("https://www.wikipedia.com")
+    expect(top_links.length).to eq(10)
+  end
 end
