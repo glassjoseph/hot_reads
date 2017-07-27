1d9d45f0d487f7c02e2fc565c751f70cab6af796
glassjoseph <glassjoseph@gmail.com>
2017-07-27 14:49:04 -0600

    Add create method for ReadsController

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::ReadsController < ApplicationController
 
+  def create
+    read = Read.new(read_params)
+    if read.save
+      render json: {status: 201}
+    else
+      render json: {status: 400}
+    end
+  end
+
+
+  def read_params
+    params.permit(:url)
+  end
 
 end
